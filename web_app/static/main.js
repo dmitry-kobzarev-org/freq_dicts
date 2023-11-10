0e93c705da460b2a588ffb64b076f4f58e27007d
Dmitry <kobzaraydzmitry@gmail.com>
2023-11-10 13:17:45 +0100

    remove scrolling in mobile screen

--- web_app/static/main.js
+++ web_app/static/main.js
@@ -21,30 +21,31 @@ class DataHandler {
     constructor() {
         this.leftButton = document.getElementById('left_button_id');
         this.rightButton = document.getElementById('right_button_id');
-        this.infoBlock = document.getElementById('info_block_id');
-        this.audio = document.getElementById('info_block_audio_id');
-        this.repeatButton = document.getElementById('info_block_repeat_id');
-        this.showWord = document.getElementById('info_block_show_id');
+        this.container = document.getElementById('container_id');
+        this.audio = document.getElementById('audio_id');
+        this.repeat = document.getElementById('repeat_id');
+        this.text = document.getElementById('text_id');
         this.data_batch_size = 20;
         this.data_rest_minimum = 10;
         this.updating_is_running = false;
         this.items_are_modifying = false;
+        this.start_is_clicked = false;
         // Attach click event handlers to the buttons
         this.leftButton.addEventListener('click', this.handleLeftButtonClick.bind(this));
         this.rightButton.addEventListener('click', this.handleRightButtonClick.bind(this));
-        this.repeatButton.addEventListener('click', this.handleRepeatButtonClick.bind(this));
-        this.showWord.addEventListener('click', this.handleShowWordClick.bind(this));
+        this.repeat.addEventListener('click', this.handleRepeatClick.bind(this));
+        this.text.addEventListener('click', this.handleTextClick.bind(this));
     }
         // Load data when the page is loaded
     async start() {
-        const loadingAnimation = this.addLoadingAnimation();
+        // const loadingAnimation = this.addLoadingAnimation();
         try {
           await this.updateItems(true);
           if (this.items.length > 0) {
                 this.counter = 0;
                 this.rest_counter = this.items.length - this.counter - 1
-                this.removeLoadingAnimation(loadingAnimation);
-                this.updateInfoBlock(true);
+                this.removeLoadingAnimation();
+                this.updateInfo(true);
             }
             
         } catch (error) {
@@ -55,12 +56,12 @@ class DataHandler {
     addLoadingAnimation() {
         const loadingAnimation = document.createElement('div');
         loadingAnimation.className = 'loading-animation';
-        this.infoBlock.appendChild(loadingAnimation);
+        this.container.appendChild(loadingAnimation);
         return loadingAnimation;
     }
 
-    removeLoadingAnimation(loadingAnimation) {
-        this.infoBlock.removeChild(loadingAnimation);
+    removeLoadingAnimation() {
+        document.querySelector('.loading-overlay').style.display = 'none';
     }
 
     async updateItems(initLaunch) {
@@ -96,20 +97,28 @@ class DataHandler {
         this.handleButton('right');
     }
 
-    handleRepeatButtonClick() {
+    handleRepeatClick() {
+        if (this.start_is_clicked == false) {
+            this.repeat.textContent = 'repeat';
+            this.start_is_clicked = true;
+        }
         this.audio.play();
     }
 
-    handleShowWordClick() {
+    handleTextClick() {
+        
+        if (this.start_is_clicked == false)
+            return
+        
         if (this.word_is_shown == true)
             return;
-        this.showWord.textContent = this.items[this.counter].word;
+        this.text.textContent = this.items[this.counter].word;
         this.word_is_shown = true;
     }
 
-    refreshShowWord() {
+    refreshText() {
         this.word_is_shown = false;
-        this.showWord.textContent = 'show word'
+        this.text.textContent = 'show'
     }
     
     _print_info() {
@@ -119,14 +128,17 @@ class DataHandler {
         // print_array(this.items);
     }
 
-    updateInfoBlock() {
+    updateInfo() {
         const item = this.items[this.counter];
         this.audio.src = item.signed_url;
         this.audio.play();
-        this.refreshShowWord();
+        this.refreshText();
     }
     
     handleButton(direction) {
+
+        if (this.start_is_clicked == false)
+            return
 
         if (this.items_are_modifying == true) {
             this._print_info()
@@ -140,7 +152,7 @@ class DataHandler {
         this.items[this.counter].button = direction;
         this.counter ++;
         this.rest_counter --;
-        this.updateInfoBlock();   
+        this.updateInfo();   
         
         if (this.rest_counter < this.data_rest_minimum) {
             if (this.updating_is_running == false) {
